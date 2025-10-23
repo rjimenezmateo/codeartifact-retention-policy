5634a2bed5e3f232917d6762d3d97d69c8a2991b
Raul Jimenez Mateo <rjimenezmateo@vega.investments>
2025-10-23 11:12:47 +0200

    feat(naming): adding missing rule_name_prefix var to the module instance

--- codeartifact-retention-module/schedules.tf
+++ codeartifact-retention-module/schedules.tf
@@ -6,4 +6,5 @@ module "codeartifact_retention_schedule" {
   repository_arn = var.repositories[count.index].repository_arn
   days_to_retain = var.repositories[count.index].days_to_retain
   lambda_arn = aws_lambda_function.codeartifact_retention.arn
+  rule_name_prefix = var.rule_name_prefix
 }
